d6686732f327c26d58e319bf0fa1c1b99e2c9b7f
rolan <rolan@ROLAND-PC>
2022-04-09 17:15:43 +0200

    Add project files.

---
+++ Decoder.Cli/Options.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoder.Cli
+{
+    internal class Options
+    {
+        [Option('j', "jwt", Required = false, HelpText = "Decode JWT token.")]
+        public string Jwt { get; set; }
+
+        [Option('b', "base64", Required = false, HelpText = "Decode base64 string.")]
+        public string Base64 { get; set; }
+
+        [Option('u', "url", Required = false, HelpText = "Decode url string.")]
+        public string Url { get; set; }
+
+        [Option('e', "encode", Required = false, HelpText = "Flips decode to encode where possible.")]
+        public bool Encode { get; set; }
+    }
+}
---
+++ Decoder.Cli/Program.cs
@@ -0,0 +1,82 @@
+﻿
+using CommandLine;
+using CommandLine.Text;
+using Decoder.Cli;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Text.Json;
+
+var parser = new Parser(with => with.HelpWriter = null);
+var parserResult = parser.ParseArguments<Options>(args);
+if (args.Length == 0) DisplayHelp(parserResult);
+
+parserResult
+  .WithParsed<Options>(o =>
+  {
+      if (!string.IsNullOrEmpty(o.Jwt))
+          DecodeJwt(o.Jwt);
+      else if (!string.IsNullOrEmpty(o.Base64))
+          ParseBase64(o.Base64, o.Encode);
+      else if (!string.IsNullOrEmpty(o.Url))
+          ParseUrl(o.Url, o.Encode);
+      else
+          throw new ArgumentException("No input provided");
+  })
+  .WithNotParsed(errs => DisplayHelp(parserResult));
+
+
+void DisplayHelp<T>(ParserResult<T> result)
+{
+    var helpText = HelpText.AutoBuild(result, h => HelpText.DefaultParsingErrorsHandler(result, h), e => e);
+    Console.WriteLine(helpText);
+    Environment.Exit(1);
+}
+
+void ParseUrl(string url, bool encode)
+{
+    string output = string.Empty;
+    if (encode)
+        output = Uri.EscapeDataString(url);
+    else
+        output = Uri.UnescapeDataString(url);
+
+    Console.WriteLine(output);
+    CopyToClipboard(output);
+}
+
+void ParseBase64(string base64, bool encode)
+{
+    string output = string.Empty;
+    if (encode)
+        output = Convert.ToBase64String(Encoding.UTF8.GetBytes(base64));
+    else
+        output = Encoding.UTF8.GetString(Convert.FromBase64String(base64));
+
+    Console.WriteLine(output);
+    CopyToClipboard(output);
+}
+
+void DecodeJwt(string jwt)
+{
+    var handler = new JwtSecurityTokenHandler();
+    var jsonToken = handler.ReadToken(jwt);
+    var tokenS = jsonToken as JwtSecurityToken;
+
+    if (tokenS == null)
+    {
+        Console.WriteLine("Invalid JWT");
+        return;
+    }
+    var json = JsonSerializer.Serialize(tokenS.Payload, new JsonSerializerOptions
+    {
+        WriteIndented = true
+    });
+    Console.WriteLine(json);
+    CopyToClipboard(json);
+}
+
+void CopyToClipboard(string text)
+{
+    TextCopy.ClipboardService.SetText(text);
+}
+
